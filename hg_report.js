8a6941fa3782adbf3f309e055992e9c99056c977
Anthony Arias <aarias011@west-mec.org>
2019-03-04 16:40:59 -0700

    Final commit push. ready for review
    Today I fixed a couple of mistakes i had to fully finish the website to make put in the quotes of the rendered page.

--- hg_report.js
+++ hg_report.js
@@ -12,7 +12,7 @@
 	
 */
 //writes information about the game that will be displayed in the web page.
-var gameReport = "<h1>" + itemTitle + "</h1> <h2>By: " + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "' id=”gameImg” /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr>  <tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
+var gameReport = "<h1>" + itemTitle + "</h1> <h2>By: " + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg' /> <table><tr><th>Product ID</th><td>" + itemID + "</td> </tr> <tr> <th>List Price</th> <td>" + itemPrice + "</td> </tr> <tr> <th>Platform</th> <td>" + itemPlatform + "</td> </tr> <tr> <th>ESRB Rating</th> <td>" + itemESRB + "</td></tr><tr> <th>Condition</th> <td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
 
 document.getElementsByTagName("article")[0].innerHTML = gameReport;
 
@@ -22,20 +22,25 @@ var ratingsSum = 0;
 //sets the ratingsCount variable equal to the ratings array
 var ratingsCount = ratings.length;
 
-//loops through the contents of the ratings array
-for (var i = 0; i < ratings.length; i = ratings + ratingsSum) {
-    var ratingsAvg = ratingsSum / ratingsCount;
-    var ratingReport = "<h1>Customer Reviews</h1> <h2> " + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
+//first for loop of the ratings array
+for (var i = 0; i <= ratingsCount; i++) {
+    ratingsSum += ratings[i];
 }
 
+//this determines the rating average
+var ratingsAvg = ratingsSum / ratingsCount;
+//this sets html to the ratingReport variable
+var ratingReport = "<h1>Customer Reviews</h1> <h2> " + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
+
+//second for loop displays the content of the first three customer reviews 
 for (var i = 0; i <= 2; i++) {
-    var ratingReport = "div class='review'> <h1>" + ratingTitles + "</h1><table> <tr><th>By</th><td>" + ratingAuthors + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates + "</td></tr> <tr><th>Rating</th><td>";
+    ratingReport += "<div class='review'> <h1>" + ratingTitles[i] + "</h1><table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
 
-    for (var i = 0; i <= 4; i++) {
-        var ratingReport = "<img src=”hg_star.png” />"
+    for (var j = 1; j <= ratings[i]; j++) {
+        ratingReport += "<img src='hg_star.png' />";
     }
-
-    var ratingReport = "</td></tr></table>" + ratingSummaries + "</div>";
+    ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
 }
 
+//this code calls back the html code into the javascript
 document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
